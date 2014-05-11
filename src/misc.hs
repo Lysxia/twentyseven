33f22cce607fd2ca9a3d5c0ddbc22b78098494c9
Syrak <li-yao.xia@ens.fr>
2014-05-11 15:55:34 +0200

    Documentation: Misc

--- src/misc.hs
+++ src/misc.hs
@@ -1,36 +1,67 @@
+{- |
+   General functions for the __twentyseven__ project
+-}
+
 module Misc where
+
+--import Math.Combinatorics.Exact.Binomial ( choose )
 
 import Data.Maybe
 import Data.List
 import Data.Array.IArray
 import Data.Array.Unboxed
 
--- Lists
+-- * Lists
 
+-- | Rotation: @rotate 3 [1 .. 7] == [4 .. 7] ++ [1 .. 3]@
 rotate :: Int -> [a] -> [a]
 rotate n l = l2 ++ l1
   where (l1, l2) = splitAt n l
 
+-- | Substitute the @n@-th element
+subs :: Int -> a -> [a] -> [a]
 subs 0 x (a : as) = x : as
-subs n x (a : as) = a : subs (n-1) x as
+subs n x (a : as) = a : subs (n - 1) x as
 
--- n = length l
-inverseList n l = [fromJust $ elemIndex i l | i <- [0..n - 1]]
+-- | If @l@ is a permutation list of length @n@,
+-- @inverseList n l@ is its inverse permutation.
+inverseList :: Int -> [Int] -> [Int]
+inverseList n l = [fromJust $ elemIndex i l | i <- [0 .. n - 1]]
 
+composeList :: [Int] -> [Int] -> [Int]
 composeList = map . (!!)
 
--- Arrays
+-- * Arrays
 
+-- | @idArray r ! i = i@
 idArray :: (IArray a i, Ix i) => (i, i) -> a i i
 idArray r = listArray r $ range r
 
-inverseArray c = array (bounds c) $ map (\(x,y) -> (y,x)) $ assocs c
+-- | If @c@ is a permutation, @inverseArray c@ is its inverse permutation
+-- (replaced-by representation)
+inverseArray :: (IArray a i, Ix i) => a i i -> a i i
+inverseArray c = array (bounds c) $ map (\(x, y) -> (y, x)) $ assocs c
 
+-- | Permutation composition
 composeArray :: (IArray a i, Ix i) => a i i -> a i i -> a i i
 composeArray a b = ixmap (bounds b) (b !) a
 
--- Groups
+-- * Groups
 
+infixl 7 ?
+infixr 8 ?^
+
+-- | Class for groups
+--
+-- @
+--   a ? (b ? c) == (a ? b) ? c -- Associative property
+--
+--   a ? iden == a -- Neutral element
+--   iden ? a == a
+--
+--   a ? inverse a == iden -- Inverse
+--   inverse a ? a == iden
+-- @
 class Group a where
   iden :: a
   inverse :: a -> a
@@ -41,20 +72,30 @@ instance (Group a, Group b) => Group (a, b) where
   inverse (a, b) = (inverse a, inverse b)
   (a1, b1) ? (a2, b2) = (a1 ? a2, b1 ? b2)
 
-(?^) :: Group a => a -> Int -> a
+-- | Exponentiation. Negative indices are supported.
+(?^) :: (Integral int, Group a) => a -> int -> a
 _ ?^ 0 = iden
 a ?^ 1 = a
-a ?^ n | n > 0     = a ? (a ?^ (n-1))
-       | otherwise = (inverse a) ?^ (-n)
+a ?^ n
+ | n < 0          = inverse a ?^ (-n)
+ | n `mod` 2 == 0 = a2
+ | otherwise      = a ? a2
+ where a2 = a_n_2 ? a_n_2
+       a_n_2 = a ?^ (n - 1)
 
+-- | Conjugation: @s ?? a = inverse s ? a ? s@
 (??) :: Group a => a -> a -> a
 s ?? a = inverse s ? a ? s
 
--- Combinatorics
+-- * Combinatorics
 
+-- | Factorial
+fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact (n - 1)
 
+-- | Binomial coefficient:
+-- @choose n k == fact n `div` (fact k) * (fact (n - k))@
 choose :: Int -> Int -> Int
 choose = \n k -> if k < 0 then 0 else c !! n ! k
   where c = [listArray (0,n) $ line n | n <- [0..]] :: [UArray Int Int]
@@ -64,4 +105,3 @@ choose = \n k -> if k < 0 then 0 else c !! n ! k
             then return 1
             else let cn = c !! (n - 1) in
                  return $ cn ! k + cn ! (k - 1)
- 
