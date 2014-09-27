980161db2a7a053c153f9920912bfe8f3db36ca3
Lysxia <li-yao.xia@ens.fr>
2014-09-27 21:57:13 +0200

    Add executables

---
+++ src/tstables.hs
@@ -0,0 +1,33 @@
+import TwoPhase
+
+import Control.Applicative
+
+import System.Directory
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.IO
+
+main :: IO ()
+main = do
+  args <- getArgs
+  path <- case args of
+        [] -> (</> ".tseven") <$> getHomeDirectory
+        p : _ -> return p
+  createDirectoryIfMissing
+    True -- createParents
+    path
+  let p1 = path </> "phase1"
+      p2 = path </> "phase2"
+  fileExists <- and <$> mapM doesFileExist [p1,p2]
+  case fileExists of
+    True -> do
+      hPutStrLn stderr $ "File already exists in '" ++ path ++"'."
+      exitFailure
+    False -> do
+      putStrLn "Phase 1"
+      encodeFile p1 phase1Compressed'
+      putStrLn "Phase 2"
+      encodeFile p2 phase2Compressed'
+      exitSuccess
+
---
+++ src/twophase.hs
@@ -0,0 +1,26 @@
+import Misc
+import Cubie
+import Moves
+import TwoPhase
+
+import Control.Applicative
+
+import Data.List
+
+import System.Directory
+import System.Environment
+import System.FilePath
+
+main :: IO ()
+main = do
+  args <- getArgs
+  path <- case args of
+        [] -> (</> ".tseven") <$> getHomeDirectory
+        p : _ -> return p
+  ph1 <- phase1Expand <$> decodeFile (path </> "phase1")
+  ph2 <- phase2Expand <$> decodeFile (path </> "phase2")
+  let solve = twoPhase (phase1 ph1) (phase2 ph2)
+  ph1 `seq` ph2 `seq` putStrLn "Ready."
+  c <- (foldl' (?) iden . map decodeMove) <$> getLine
+  print ((intercalate " " . map snd) <$> solve c)
+
