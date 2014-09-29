1cfce13d91fbfe505584e3382dc20f5db5de87b1
Lysxia <li-yao.xia@ens.fr>
2014-09-29 02:02:29 +0200

    File storage wrapper

--- src/Tables.hs
+++ src/Tables.hs
@@ -14,15 +14,38 @@ import Symmetry
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Reader
 
+import Data.Binary
 import qualified Data.Vector.Unboxed as U
+
+import System.FilePath
+
+data Stored a = Stored {
+  store    :: IO (),
+  retrieve :: IO a
+  }
+
+-- | The @Reader@ monad wraps the path to the root directory for
+-- the program.
+-- The preceding @FilePath@ argument is the name of the file to store
+-- the first argument to, and later to read an equal value from.
+mkStored :: Binary a => a -> FilePath -> Reader FilePath (Stored a)
+mkStored a filename = ReaderT $ \path ->
+  let filepath = path </> filename
+  in return $ Stored {
+      store = encodeFile filepath a,
+      retrieve = decodeFile filepath
+    }
+
+
+
+moveTables :: CubeAction a => [Cube] -> Coordinate a -> [Vector Coord]
+moveTables moves coord = (endoVector coord . flip cubeAction) <$> moves
 
 symClassesFlipUDSlice :: Vector SymCoord
 symClassesFlipUDSlice
   = symClasses
       coordFlipUDSlice
       (map conjugateFlipUDSlice sym16)
-
-moveTables :: CubeAction a => [Cube] -> Coordinate a -> [Vector Coord]
-moveTables moves coord = (endoVector coord . flip cubeAction) <$> moves
 
