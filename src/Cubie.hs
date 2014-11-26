86dbc68c2de36fb415a1e731423558e074a7131c
Lysxia <li-yao.xia@ens.fr>
2014-11-26 22:42:41 +0100

    Fix Haddock-breaking comment

--- src/Cubie.hs
+++ src/Cubie.hs
@@ -346,7 +346,7 @@ solvable (Cube (Corner (CornerPermu cp) (CornerOrien co))
   signPermutationVector cp == signPermutationVector ep
   && U.sum co `mod` 3 == 0
   && U.all (< 3) co
-  -- ^ the data structure allows to encode all 6 permutations of the 3 facelets
+  -- Above: the data structure allows to encode all 6 permutations of the 3 facelets
   -- so we need to exclude the 3 transpositions, which are represented by 3, 4, 5.
   && U.sum eo `mod` 2 == 0
 
