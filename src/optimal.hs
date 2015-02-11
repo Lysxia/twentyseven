01112f2950a902479e87cd2a1dbbbcdbb46e2fa2
Lysxia <li-yao.xia@ens.fr>
2015-02-11 23:33:09 +0100

    Add optimal solver

---
+++ src/optimal.hs
@@ -0,0 +1,157 @@
+import Rubik.Cube
+import Rubik.Misc
+import Rubik.Solver
+import Rubik.Solver.Optimal
+
+import Control.Applicative
+import Control.Exception
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Reader
+
+import Criterion.Measurement ( getCPUTime, secs )
+
+import Data.Function
+import Data.List
+import Data.Maybe
+import Data.Monoid
+
+import System.Environment
+import System.Exit
+import System.IO
+import System.IO.Error
+
+data Parameters = Parameters {
+    quiet :: Bool,
+    preload :: Bool
+  }
+
+defaultParam = Parameters {
+    quiet = False,
+    preload = False
+  }
+
+unlessQuiet :: Monad m => m () -> ReaderT Parameters m ()
+unlessQuiet a =
+  ask >>= \p -> unless (quiet p) (lift a)
+
+-- Strict in its second argument
+uQ :: Monad m => (a -> m ()) -> a -> ReaderT Parameters m ()
+uQ f a
+  = a `seq` unlessQuiet (f a)
+
+type P = ReaderT Parameters
+
+clock :: a -> IO Double
+{-# NOINLINE clock #-}
+clock x = do
+  t <- getCPUTime
+  evaluate x
+  t' <- getCPUTime
+  return $ t' - t
+
+qPutStrLn :: String -> P IO ()
+qPutStrLn s = do
+  p <- ask
+  lift $ if quiet p
+    then evaluate (s `listSeq` ())
+    else putStrLn s
+
+qPutStr :: String -> P IO ()
+qPutStr s = do
+  p <- ask
+  lift $ if quiet p
+    then evaluate (s `listSeq` ())
+    else putStr s
+
+doPreload :: P IO ()
+{-# NOINLINE doPreload #-}
+doPreload = do
+  qPutStrLn "Moves."
+  t <- lift $ getCPUTime
+  qPutStr "CornerPermu" >> clockCI move18CornerPermu
+  qPutStr "CornerOrien" >> clockCI move18CornerOrien
+  qPutStr "EdgeOrien" >> clockCI move18EdgeOrien
+  qPutStr "UDSlice" >> clockCI move18UDSlice
+  qPutStr "LRSlice" >> clockCI move18LRSlice
+  qPutStr "FBSlice" >> clockCI move18FBSlice
+  qPutStr "UDSlicePermu" >> clockCI move18UDSlicePermu
+  qPutStr "LRSlicePermu" >> clockCI move18LRSlicePermu
+  qPutStr "FBSlicePermu" >> clockCI move18FBSlicePermu
+  qPutStr "dist_CornerPermu" >> clockPrint dist_CornerPermu
+  qPutStr "dist_CornerOrien_UDSlice" >> clockPrint dist_CornerOrien_UDSlice
+  qPutStr "dist_CornerOrien_LRSlice" >> clockPrint dist_CornerOrien_LRSlice
+  qPutStr "dist_CornerOrien_FBSlice" >> clockPrint dist_CornerOrien_FBSlice
+  t' <- lift $ getCPUTime
+  qPutStrLn $ "Total: " ++ secs (t' - t)
+  where
+    clockCI l = clockPrint (movesCI l `listSeq` ())
+    clockPrint x = do
+      t <- lift $ clock x
+      qPutStrLn $ ": " ++ secs t
+
+main :: IO ()
+main = do
+  (p, args') <- prepareArgs defaultParam
+  let arg = filter (/= ' ') . concat $ args'
+  when (preload p) $ runReaderT doPreload p
+  case arg of
+    [] -> do
+      catchIOError
+        (forever $ do
+          s <- filter (/= ' ') <$> getLine
+          if s == ""
+          then exitSuccess
+          else runReaderT (answer s) p)
+        (\e -> if isEOFError e then return () else ioError e)
+    s -> runReaderT (answer s) p
+
+prepareArgs :: Parameters -> IO (Parameters, [String])
+prepareArgs p = prepare p <$> getArgs
+  where
+    prepare p ("-p" : args) = prepare (p { preload = True }) args
+    prepare p ("-q" : args) = prepare (p { quiet = True }) args
+    prepare p args = (p, args)
+
+answer :: String -> ReaderT Parameters IO ()
+answer s = do
+  case s of
+    '.' : s' -> uQ moveSequence s'
+    "random" -> uQ putStrLn =<< stringOfCubeColors <$> lift randomCube
+    "solverandom" -> do
+      c <- lift randomCube
+      uQ putStrLn (stringOfCubeColors c)
+      justSolve c
+    _ -> faceletList s
+
+-- A sequence of moves, e.g., "URF".
+moveSequence s = putStrLn $
+  case stringToMove s of
+    Left c -> "Unexpected '" ++ [c] ++ "'."
+    Right ms -> stringOfCubeColors . moveToCube . reduceMove $ ms
+
+faceletList = either (unlessQuiet . putStrLn) justSolve . readCube
+
+readCube s
+  = case normalize s of
+      Nothing -> Left "Expected string of length 54 of a set of (any) 6 \
+                      \characters. Centers must be distinct."
+      Just colors ->
+        case colorFaceletsToCube colors of
+          Left fs ->
+            Left $ "Facelets " ++ show fs
+                ++ " (" ++ show (map (s !!) fs) ++ ") \
+                   \do not match any regular cubie."
+          Right Nothing ->
+            Left "Not a permutation of cubies \
+                 \(a cubie is absent, and a cubie occurs twice)."
+          Right (Just c) | solvable c -> Right c
+          _ -> Left "Unsolvable cube."
+
+justSolve c
+  = if c <> moveToCube solve == iden
+    then uQ putStrLn . moveToString $ solve
+    else fail ("Incorrect solver: " ++ moveToString solve)
+  where
+    solve = optim c
+
